504903f15a2a01697aa5097f308907dafca02cea
Nykyta Koblianskyi <nkoblianslyi@gmail.com>
2024-05-02 13:06:45 +0200

    admin

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -4,6 +4,7 @@ import { Admin, DataProvider, EditGuesser, ListGuesser, Loading, Resource } from
 import buildHasuraProvider from 'ra-data-hasura';
 import { MenuList } from './modules/menu/components/menu-lists/menu-list.component';
 import { MenuEdit } from './modules/menu/components/menu-edit/menu-edit.component';
+import { MenuCreate } from './modules/menu/components/menu-create/menu-create.component';
 
 export const App = () => {
   const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(null);
@@ -27,7 +28,7 @@ export const App = () => {
     <>
       <CssBaseline />
       <Admin dataProvider={dataProvider}>
-        <Resource name="menu" list={MenuList} edit={MenuEdit}/>
+        <Resource name="menu" list={MenuList} edit={MenuEdit} create={MenuCreate}/>
       </Admin>
     </>
   );
